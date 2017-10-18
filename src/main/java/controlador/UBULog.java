e58c4edf31981c013aaa249d28e2e770c1214513
Oscar Fernandez <trona85@hotmail.es>
2017-10-18 18:06:03 +0200

    Recoger token. [#10] - Se ha creado una clase para la consulta del web service. - Generación de una clase con atributos estáticos que son las funciones del web

--- src/main/java/controlador/UBULog.java
+++ src/main/java/controlador/UBULog.java
@@ -3,7 +3,12 @@
  */
 package controlador;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import modelo.MoodleUser;
 import parserdocument.CsvParser;
+import webservice.Session;
 
 /**
  * @author oscar
@@ -11,14 +16,29 @@ import parserdocument.CsvParser;
  */
 public class UBULog {
 
+	public static String host = "http://localhost/moodle/";
+	public static Session session;
+	public static MoodleUser user;
+
+	//static final Logger logger = LoggerFactory.getLogger(UBULog.class);
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// IDocumentParser p = new CsvParser();
-		System.out.println("empezamos");
-		CsvParser p = new CsvParser("doc/docparser/logs_curso2_20171012-1005.csv");
-		p.readDocument();
+		//logger.info("[Bienvenido a UBUGrades]");
+
+		session = new Session("profesor", "1Qwerty--");
+		try {
+			session.setToken();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("user: " + session.getUserName() + ", token: "+ session.getToken());
+		
+		//TODO
+		//CsvParser p = new CsvParser("doc/docparser/logs_curso2_20171012-1005.csv");
+		//p.readDocument();
 
 	}
 
---
+++ src/main/java/webservice/Session.java
@@ -0,0 +1,110 @@
+/**
+ * 
+ */
+package webservice;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import controlador.UBULog;
+import modelo.Course;
+
+/**
+ * @author oscar
+ *
+ */
+public class Session {
+
+	private String userName;
+	private String password;
+	private String tokenUser;
+	private Course actualCourse;
+
+	/**
+	 * Constructor de la clase Session
+	 * 
+	 * @param userName
+	 *            correo del usuario
+	 * @param pass
+	 *            contraseña de usuario
+	 */
+	public Session(String userName, String pass) {
+		this.userName = userName;
+		this.password = pass;
+	}
+
+	/**
+	 * Obtiene el token de usuario
+	 * 
+	 * @return
+	 */
+	public String getToken() {
+		return this.tokenUser;
+	}
+
+	/**
+	 * Establece el token del usuario a partir de usuario y contrase�a. Se
+	 * realiza mediante una petici�n http al webservice de Moodle
+	 * 
+	 * @throws Exception
+	 */
+	public void setToken() throws Exception {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpGet httpget = new HttpGet(UBULog.host + "/login/token.php?username=" + this.userName + "&password="
+					+ this.password + "&service=" + WebServiceOptions.SERVICIO_WEB_MOODLE);
+			CloseableHttpResponse response = httpclient.execute(httpget);
+			try {
+				String respuesta = EntityUtils.toString(response.getEntity());
+				JSONObject jsonObject = new JSONObject(respuesta);
+				if (jsonObject != null) {
+					this.tokenUser = jsonObject.getString("token");
+				}
+			} finally {
+				response.close();
+			}
+		} finally {
+			httpclient.close();
+		}
+	}
+
+	/**
+	 * Devuelve el email del usuario
+	 * 
+	 * @return email
+	 */
+	public String getUserName() {
+		return this.userName;
+	}
+
+	/**
+	 * Modifica el email del usuario
+	 * 
+	 * @param userName
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * Devuelve el curso actual
+	 * 
+	 * @return actualCourse
+	 */
+	public Course getActualCourse() {
+		return this.actualCourse;
+	}
+
+	/**
+	 * Modifica el curso actual
+	 * 
+	 * @param course
+	 */
+	public void setActualCourse(Course course) {
+		this.actualCourse = course;
+	}
+}
